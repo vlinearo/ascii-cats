07fa8fbedccf871f05a3b88e880fabc24f746ec7
vlinearo <vlinearo@gmail.com>
2025-01-15 14:12:40 +0200

    error handling: add color_eyre

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#[allow(
+    dead_code,
+    unused_imports,
+    unused_variables
+)]
+
 use tokio::*;
 use reqwest::get;
 use serde::Deserialize;
+use color_eyre::{self, eyre::Ok};
 
 
 #[derive(Deserialize)]
@@ -13,10 +20,24 @@ struct CatImage {
 
 #[tokio::main]
 async fn main() {
+    let url = get_asciicat_url().await.unwrap();
+    println!("URL: {}", url);
+}
+
+
+async fn get_asciicat_url() -> color_eyre::Result<String> {
     let res = get("https://api.thecatapi.com/v1/images/search")
-        .await.expect("Request was failed");
-    let images: Vec<CatImage> = res.json()
-        .await.unwrap();
-    let image = images.first().unwrap();
-    println!("{}", image.url)
+        .await?;
+    if !res.status().is_success() {
+        return Err(color_eyre::eyre::eyre!(
+            "The Cat API returned HTTP {}",
+            res.status()
+        ));
+    }
+    let images: Vec<CatImage> = res.json().await?;
+    let Some(image) =  images.first() else {
+        return Err( color_eyre::eyre::eyre!("Cant get url from cat image!")
+        )
+    };
+    Ok(image.url.clone())
 }
