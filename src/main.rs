fceb6d9e58e63b0b56a7200326c2bce94e62d02d
vlinearo <vlinearo@gmail.com>
2025-01-15 14:36:45 +0200

    finally: art of ascii cats

--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,24 @@
 )]
 
 use tokio::*;
-use reqwest::{get, Client};
+use reqwest::Client;
 use serde::Deserialize;
-use color_eyre::{self, eyre::Ok, owo_colors::OwoColorize};
-use pretty_hex::PrettyHex;
+use color_eyre::{self, eyre::Ok};
+use image::load_from_memory;
+use artem;
 
 #[derive(Deserialize)]
 struct CatImage {
-    id: String,
     url: String,
-    width: usize,
-    height: usize
 }
 
 #[tokio::main]
 async fn main() {
-    let url = get_asciicat_url().await.unwrap();
-    println!("URL: {}", url);
-    let image_bytes = get_asciicats_bytes().await.unwrap();
-    println!("{}", &image_bytes[..200].hex_dump());
+    let cat = get_acii_cat().await.unwrap();
+    println!("{}", cat)
 }
 
-
-async fn get_asciicat_url() -> color_eyre::Result<String> {
-    let res = get("https://api.thecatapi.com/v1/images/search")
-        .await?;
-    if !res.status().is_success() {
-        return Err(color_eyre::eyre::eyre!(
-            "The Cat API returned HTTP {}",
-            res.status()
-        ));
-    }
-    let images: Vec<CatImage> = res.json().await?;
-    let Some(image) =  images.first() else {
-        return Err( color_eyre::eyre::eyre!("Cant get url from cat image!")
-        )
-    };
-    Ok(image.url.clone())
-}
-
-async fn get_asciicats_bytes() -> color_eyre::Result<Vec<u8>> {
+async fn get_acii_cat() -> color_eyre::Result<String> {
     let client = Client::default();
     let image = client
         .get("https://api.thecatapi.com/v1/images/search")
@@ -55,8 +33,12 @@ async fn get_asciicats_bytes() -> color_eyre::Result<Vec<u8>> {
         .pop()
         .ok_or_else(|| color_eyre::eyre::eyre!("Error while getting cat image: "))?;
 
-    Ok(client
-        .get(image.url)
-        .send()
-        .await?.error_for_status()?.bytes().await?.to_vec())
+    let bytes = client.get(image.url)
+        .send().await?
+        .error_for_status()?.bytes().await?;
+
+    let image = load_from_memory(&bytes)?;
+    let ascii_cat = artem::convert(image, &artem::ConfigBuilder::new().build());
+
+    Ok(ascii_cat)
 }
